f2d15dd70e88763d1b03dc04b948f9826cc52272
Deivid Cesar <36942232+Deividcl@users.noreply.github.com>
2018-10-20 16:58:48 -0300

    Add files via upload

---
+++ ProvaIMC/ProvaIMC/IMC.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaIMC
+{
+    class IMC
+    {
+        private Double peso;
+        private double altura;
+
+        public IMC()
+        {
+            this.peso = 0.0;
+            this.altura = 0.0;
+
+        }
+
+        public IMC(double peso, double altura)
+        {
+
+            this.peso = peso;
+            this.altura = altura;
+
+        }
+
+        public double calcular(double peso, double altura)
+        {
+            double imc = ((peso) / (altura * altura));
+            return imc;
+        }
+
+        public string classificar(double IMC)
+        {
+            if (IMC < 16 && IMC > 0)
+            {
+                return "MAGREZA GRAVE";
+            }
+            else if (IMC >= 16 && IMC < 17)
+            {
+                return "Magreza Moderada";
+            }
+            else if (IMC >= 17 && IMC < 18.5)
+            {
+                return "Magreza leve";
+            }
+            else if (IMC >= 18.5 && IMC < 25)
+            {
+                return "Saudável";
+            }
+            else if (IMC >= 25 && IMC < 30)
+            {
+                return "Sobrepeso";
+            }
+            else if (IMC >= 30 && IMC < 35)
+            {
+                return "OBESIDADE GRAU |";
+            }
+            else if (IMC >= 35 && IMC < 40)
+            {
+                return "OBESIDADE GRAU ||";
+            }
+            else if (IMC > 40)
+            {
+                return "OBESIDADE GRAU |||";
+            }
+
+            return "erro";
+        }
+    }
+}
---
+++ ProvaIMC/ProvaIMC/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaIMC
+{
+    class Program
+    {
+        //Questão 1
+        static int Maior2(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+        //Questão 3
+        static bool Diferente(int x)
+        {
+            if (x % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+
+        {
+              //Questão 1 */
+
+               Console.WriteLine(Maior2(8, 6));
+               Console.ReadKey();
+             //Questao 3
+               Console.WriteLine(Diferente(7));
+               Console.ReadKey();
+
+              //IMC questao 2
+
+                IMC IMCOBJETO = new IMC();
+                Console.Write("digite o valor da sua altura :");
+                double altura = double.Parse(Console.ReadLine());
+
+                Console.Write("Digite o valor do seu peso :");
+                double peso = double.Parse(Console.ReadLine());
+
+                Double seuIMC = IMCOBJETO.calcular(peso, altura);
+
+                Console.WriteLine("Seu IMC é :" + seuIMC);
+                Console.WriteLine("Sua classificação é :" + IMCOBJETO.classificar(IMCOBJETO.calcular(peso, altura)));
+                Console.ReadKey();          
+        }
+    }
+}
